e881ee6e0ede1d415014d0ac3ea810fc53b0cc26
Austin <bintifield@gmail.com>
2024-01-09 00:44:39 +0300

    bullets in development

--- executables/bullets/Air.py
+++ executables/bullets/Air.py
@@ -9,28 +9,30 @@ from executables.bullets.Bullet import Bullet
 class Air(Bullet):
     def __init__(self, r, start_pos, end_pos, *sprite_groups):
         super().__init__(r, start_pos, end_pos, *sprite_groups)
-        self.start_radius = int(500 * self.r.constant("coefficient"))
-        self.detalization = math.pi / 24
-        self.angles = list()
+        self.wavefront_threshold = int(500 * self.r.constant("coefficient"))
+        detalization = math.pi / 48
+        start_angle = self.angle - math.pi / 16
+        end_angle = self.angle + math.pi / 16
+        self.angles = [start_angle]
+        while (an := self.angles[-1] + detalization) < end_angle:
+            self.angles.append(an)
+        self.points = list()
+        self.points_history = list()
+        self.count_of_drawcalls = int()
+
+    def calculate_points(self):
+        self.points = [(self.start_pos[0] + math.cos(elem) * self.speed * self.lifetime(),
+                        self.start_pos[1] - math.sin(elem) * self.speed * self.lifetime())
+                       for elem in self.angles]
+        self.points_history.append(self.points)
+        self.end_pos = self.points[len(self.points) // 2]
 
     def draw(self, surface):
-        if not self.angles:
-            angle = math.atan(abs(self.delta_y / self.delta_x))
-            if self.delta_x < 0 < self.delta_y:
-                angle = math.pi - angle
-            if self.delta_y < 0 < self.delta_x:
-                angle = 2 * math.pi - angle
-            if self.delta_x < 0 and self.delta_y < 0:
-                angle = angle + math.pi
-            angle = 2 * math.pi - angle
-            start_angle = angle - math.pi / 8
-            end_angle = angle + math.pi / 8
-            self.angles = [start_angle, end_angle]
-            for i in range(1, int((end_angle - start_angle) // self.detalization)):
-                self.angles.insert(-1, start_angle + i * self.detalization)
-        points = [(self.start_pos[0] + math.cos(elem) * self.start_radius * (time.time() - self.spawn_time),
-                   self.start_pos[1] - math.sin(elem) * self.start_radius * (time.time() - self.spawn_time))
-                  for elem in self.angles]
-        points.append(self.start_pos)
-        pygame.draw.polygon(surface, pygame.Color("green"), points)
+        self.calculate_points()
+        if self.speed * self.lifetime() > self.wavefront_threshold:
+            pygame.draw.polygon(surface, pygame.Color("green"), self.points +
+                                self.points_history[-self.count_of_drawcalls][::-1])
+        else:
+            self.count_of_drawcalls += 1
+            pygame.draw.polygon(surface, pygame.Color("green"), self.points + [self.start_pos])
 
--- executables/bullets/Bullet.py
+++ executables/bullets/Bullet.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 import pygame.time
@@ -7,28 +8,28 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, r, start_pos, end_pos, *sprite_groups):
         super().__init__(*sprite_groups)
         self.r = r
-        self.spawn_time = time.time()
-        self.clock = pygame.time.Clock()
+        self.spawn_time = time.time_ns() / 10 ** 6
+        self.lifetime = lambda: time.time_ns() / 10 ** 6 - self.spawn_time
         self.delta_x = end_pos[0] - start_pos[0]
         self.delta_y = end_pos[1] - start_pos[1]
-        self.current_pos = int(), int()
         self.start_pos = start_pos
         self.end_pos = end_pos
-        self.speed = 0.5
+        self.speed = 0.5 * self.r.constant("coefficient")
         self.rect = pygame.Rect(self.start_pos[0] - self.r.constant("useful_width") / 2,
                                 self.start_pos[1] - self.r.constant("useful_height") / 2,
                                 self.r.constant("useful_width"), self.r.constant("useful_height"))
+        self.angle = math.atan(abs(self.delta_y / self.delta_x))
+        if self.delta_x < 0 < self.delta_y:
+            self.angle = math.pi - self.angle
+        if self.delta_y < 0 < self.delta_x:
+            self.angle = 2 * math.pi - self.angle
+        if self.delta_x < 0 and self.delta_y < 0:
+            self.angle = self.angle + math.pi
+        self.angle = 2 * math.pi - self.angle
 
     def update(self):
-        if not (self.rect.x <= self.current_pos[0] <= self.rect.x + self.rect.width and
-                self.rect.y <= self.current_pos[1] <= self.rect.y + self.rect.height):
+        if not (self.rect.x <= self.end_pos[0] <= self.rect.x + self.rect.width and
+                self.rect.y <= self.end_pos[1] <= self.rect.y + self.rect.height):
             self.kill()
             return
-        length = self.clock.tick() * self.speed
-        x_movement = (self.delta_x ** 2 * length ** 2 / (self.delta_y ** 2 + self.delta_x ** 2)) ** 0.5 \
-            if self.delta_y else length
-        y_movement = abs(x_movement * self.delta_y / self.delta_x) if self.delta_x else length
-        self.current_pos = self.start_pos[0] + (x_movement := x_movement * (1 if self.delta_x > 0 else -1)), \
-            self.start_pos[1] + (y_movement := y_movement * (1 if self.delta_y > 0 else -1))
-        self.end_pos = self.end_pos[0] + x_movement, self.end_pos[1] + y_movement
 
