08c3d91bf23e33b4ba154b03dadc41e63cfcf7ea
Austin <bintifield@gmail.com>
2024-02-17 22:53:48 +0300

    development

--- executables/ui/Completed.py
+++ executables/ui/Completed.py
@@ -1,4 +1,10 @@
+import math
+
+import pygame.mouse
+
 from executables.ui.Screen import Screen
+from executables.ui.widgets.Button import Button
+from executables.ui.widgets.Label import Label
 
 
 class Completed(Screen):
@@ -7,6 +13,36 @@ class Completed(Screen):
         self.images = [self.r.drawable("completed_0"), self.r.drawable("completed_1"), self.r.drawable("completed_2")]
         self.images.extend(self.images[-2:-len(self.images):-1])
         self.add_time_event("animation", lambda: self.images.insert(0, self.images.pop()), 150)
+        self.position = -self.r.constant("useful_height")
+        self.add_time_event("moving", lambda: self.move(0), 35)
+        self.step = 150 * self.r.constant("coefficient")
+        floor = self.r.query("SELECT number FROM floor ").fetchall()[0][0] if False else 10
+        self.label = Label(self.r, self.r.string("floor_is_cleared").replace('%', str(floor)), (40, 500), 300, 500, "white")
+        self.buttons = (
+            Button(self.r, self.r.string("to_the_floor").replace('%', str(floor - 1)), (80, 1400),
+                   lambda: self.add_time_event("moving", lambda: self.move(self.r.constant("useful_height")), 35)),
+        )
+
+    def mouse_pressed(self, button, pos):
+        if button == 1:
+            for elem in self.buttons:
+                elem.click()
+
+    def move(self, barier):
+        if self.position < barier:
+            self.position -= min(-1, self.step * -abs(math.sin(self.position / self.r.constant("useful_height"))))
+        else:
+            self.remove_time_event("moving")
+            if self.position >= self.r.constant("useful_height"):
+                self.signal_to_change = "continued"
 
     def update(self):
-        self.frame.blit(self.images[0], (0, 0))
+        self.frame.fill("black")
+        surface = pygame.Surface(self.frame.get_rect()[-2:])
+        surface.blit(self.images[0], (0, 0))
+        self.label.draw(surface)
+        for elem in self.buttons:
+            elem.check_focus(pygame.mouse.get_pos())
+            elem.draw(surface)
+        self.frame.blit(surface, (0, self.position))
+        return self.signal_to_change
--- executables/ui/widgets/Widget.py
+++ executables/ui/widgets/Widget.py
@@ -1,9 +1,10 @@
-import pygame
-
-
 class Widget:
     def __init__(self, r, pos):
         self.r = r
+        self.x, self.y = int(), int()
+        self.set_pos(pos)
+
+    def set_pos(self, pos):
         self.x, self.y = map(lambda elem: elem * self.r.constant("coefficient"), pos)
 
     def build_surface(self):
