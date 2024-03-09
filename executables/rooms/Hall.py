1ec9bc5dbd51bc76c055e85b0479fdec04d615dc
Austin <bintifield@gmail.com>
2024-03-09 15:51:55 +0300

    hall fixed

--- executables/rooms/Hall.py
+++ executables/rooms/Hall.py
@@ -8,6 +8,7 @@ class Hall(Room):
         super().__init__(r, collection_coords, is_recreated)
         quad_surface = pygame.Surface(((w := self.r.constant("useful_width")) * 2,
                                        (h := self.r.constant("useful_height")) * 2))
+        quad_surface.fill(pygame.Color(142, 175, 154))
         quad_surface.blit(self.image, (0, 0))
         quad_surface.blit(self.image, (w, 0))
         quad_surface.blit(self.image, (0, h))
